6e3ac2c3c4417eeefe674ddc3b62ce79cd2783a0
Getvion <gog0125@mail.ru>
2021-09-29 20:22:29 +0300

    добавил запрос для ожидаемых фильмов

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
-// Добавить разделы "В кинотеатре", "Новые фильмы", "Ожидаемые"
 // Парсить данные в модальное окно
 // Поставить в banner данные о самом ожидаемом фильме, может сделать карусель
 
-// работа с api
 const API_KEY = '7dcd1d86-569b-4840-9c72-fa383b7b693a';
 const API_URL_POPULAR =
   'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=';
+const API_URL_SOON =
+  'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_AWAIT_FILMS&page=';
 
-getMovies(API_URL_POPULAR);
+// Получение популярных фильмов
+getPopularMovies(API_URL_POPULAR);
 
-async function getMovies(url) {
+async function getPopularMovies(url) {
   const resp = await fetch(url, {
     headers: {
       'Content-Type': 'application/json',
@@ -17,23 +18,72 @@ async function getMovies(url) {
     },
   });
   const respData = await resp.json();
-  showMovies(respData);
+
+  showPopularMovies(respData);
 }
 
-function showMovies(data) {
+function showPopularMovies(data) {
   const popularMovies = document.querySelector('#popular');
 
   data.films.forEach((movie) => {
     const popularMovie = document.createElement('div');
+
     popularMovie.classList.add('films__item');
+
     popularMovie.innerHTML = `
         <img src="${movie.posterUrlPreview}" alt="${movie.nameRu}" class="films__item-img">
+        <div class="films__item-rating films__item-rating--${getClassByRate(movie.rating)}">${movie.rating}</div>
         <p class="films__item-title">${movie.nameRu}</p>
         <p class="films__item-genre">${movie.genres.map(
       (genre) => ` ${genre.genre}`)}</p>
     `;
+
     popularMovies.appendChild(popularMovie);
   });
+}
+
+// получение ожидаемых фильмов
+getSoonMovies(API_URL_SOON);
+
+async function getSoonMovies(url) {
+  const resp = await fetch(url, {
+    headers: {
+      'Content-Type': 'application/json',
+      'X-API-KEY': API_KEY,
+    },
+  });
+  const respData = await resp.json();
+
+  showSoonMovies(respData);
+}
+function showSoonMovies(data) {
+  const soonMovies = document.querySelector('#soon');
+
+  data.films.forEach((movie) => {
+    const soonMovie = document.createElement('div');
+    soonMovie.classList.add('films__item');
+
+    soonMovie.innerHTML = `
+        <img src="${movie.posterUrlPreview}" alt="${movie.nameRu}" class="films__item-img">
+        <div class="films__item-rating films__item-rating--${getClassByRate(movie.rating)}">${movie.rating}</div>
+        <p class="films__item-title">${movie.nameRu}</p>
+        <p class="films__item-genre">${movie.genres.map(
+      (genre) => ` ${genre.genre}`)}</p>
+    `;
+
+    soonMovies.appendChild(soonMovie);
+  });
+}
+
+// Изменение цвета по рейтингу
+function getClassByRate(vote) {
+  if (vote >= 7 || vote >= '90') {
+    return 'green';
+  } else if (vote >= 5 || vote >= '80') {
+    return 'orange';
+  } else {
+    return 'red';
+  }
 }
 
 //Поиск 
@@ -46,30 +96,29 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   if (input.value) {
     document.querySelector('#popular').innerHTML = '';
+    document.querySelector('#soon').innerHTML = '';
     document.querySelector('.title__popular').style.display = 'none';
+    document.querySelector('.title__soon').style.display = 'none';
     document.querySelector('.banner').style.display = 'none';
-    getMovies(API_URL_SEARCH + input.value);
+    getPopularMovies(API_URL_SEARCH + input.value);
     input.value = '';
   }
 });
 
-
 // модальное окно
-const modalTrigger = document.querySelector('.show-modal');
 const modal = document.querySelector('.modal');
 const modalCloseBtn = document.querySelector('[data-close]');
 const filmsItem = document.querySelectorAll('.films__list');
 
-function openModal(event) {
-  modal.classList.toggle('show');
+function openModal() {
+  modal.style.display = 'block';
   document.body.style.overflow = 'hidden';
 }
 
 function closeModal() {
-  modal.classList.toggle('show');
+  modal.style.display = 'none';
   document.body.style.overflow = '';
 }
-modalTrigger.addEventListener('click', openModal);
 
 filmsItem.forEach(film => {
   film.addEventListener('click', openModal);
@@ -89,13 +138,12 @@ document.addEventListener('keydown', (event) => {
   }
 });
 
-
-// function getClassByRate(vote) {
+// function getClassByRateModal(vote) {
 //   if (vote >= 7) {
 //     document.querySelector('.movie__rating').classList.add('movie__rating--good');
 //   } else if (vote >= 5) {
 //     document.querySelector('.movie__rating').classList.add('movie__rating--middle');
-//   } else {
+//   } else if (vote < 5) {
 //     document.querySelector('.movie__rating').classList.add('movie__rating--bad');
 //   }
 // }
