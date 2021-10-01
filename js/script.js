890b55a8081853c02c039d9057d25d097aec147b
Getvion <gog0125@mail.ru>
2021-10-01 18:06:46 +0300

    спарсил данные в баннер, сделал баннер слайдером

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 // Парсить данные в модальное окно
-// Поставить в banner данные о самом ожидаемом фильме, может сделать карусель
-
 const API_KEY = '7dcd1d86-569b-4840-9c72-fa383b7b693a';
 const API_URL_POPULAR =
   'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=';
 const API_URL_SOON =
   'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_AWAIT_FILMS&page=';
 
+
 // Получение популярных и ожидаемых фильмов
 getMovies(API_URL_POPULAR, API_URL_SOON);
 
-async function getMovies(url, url2) {
+async function getMovies(url, url2, url3) {
   const resp = await fetch(url, {
     headers: {
       'Content-Type': 'application/json',
@@ -26,11 +25,13 @@ async function getMovies(url, url2) {
     },
   });
   const respData2 = await resp2.json();
+
   showMovies(respData, respData2);
 }
 
 const popularMovies = document.querySelector('#popular');
 const soonMovies = document.querySelector('#soon');
+const banner = document.querySelector('#banner');
 
 function myHTML(movie) {
   return `<img src="${movie.posterUrlPreview}" alt="${movie.nameRu}" class="film__item-img">
@@ -38,8 +39,23 @@ function myHTML(movie) {
     <p class="film__item-title">${movie.nameRu}</p>
     <p class="film__item-genre">${movie.genres.map((genre) => ` ${genre.genre}`)}</p>`;
 }
-
+function myBannerHTML(movie) {
+  return `
+  <div class="container">
+    <div id="banner-item" class="banner-item">
+      <img src="${movie.posterUrlPreview}" alt="${movie.nameRu}" class="banner-img">
+  <div class="banner-content">
+        <h1 class="banner-title">${movie.nameRu}</h1>
+        <p class="banner-title--orig">${movie.nameOriginal}</p>
+        <p class="banner-genre">${movie.genres.map((genre) => ` ${genre.genre}`)}</p>
+        <p class="banner-descr">${movie.description}</p>
+      </div >
+    </div >
+  </div >
+  `;
+}
 function showMovies(data, data2) {
+
   data.films.forEach((movie) => {
     const popularMovie = document.createElement('div');
     popularMovie.classList.add('film__item');
@@ -54,7 +70,47 @@ function showMovies(data, data2) {
     soonMovie.innerHTML = myHTML(movie);
     soonMovies.appendChild(soonMovie);
   });
+
+  const mostPopular = document.createElement('div');
+  mostPopular.classList.add('banner__inner');
+
+  banner.appendChild(mostPopular);
+
+  // Получение информации о фильме по его id
+  const API_URL_FILM_INFO = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/';
+  let counter = 0;
+  let filmId = data.films[counter].filmId;
+  let filmLink = API_URL_FILM_INFO + filmId;
+
+  getMovieById(filmLink);
+
+  setInterval(() => {
+    counter++;
+    console.log(counter);
+    if (counter > 19) {
+      counter = 0;
+    }
+    filmId = data.films[counter].filmId;
+    filmLink = API_URL_FILM_INFO + filmId;
+    getMovieById(filmLink);
+  }, 10000);
+
+  async function getMovieById(url) {
+    const resp = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-KEY': API_KEY,
+      },
+    });
+    const respData = await resp.json();
+    showMovieById(respData);
+  }
+
+  function showMovieById(data) {
+    mostPopular.innerHTML = myBannerHTML(data);
+  }
 }
+
 
 // Изменение цвета по рейтингу
 function getClassByRate(vote) {
@@ -110,14 +166,101 @@ function search(event) {
 form.addEventListener('submit', search);
 searchBtn.addEventListener('click', search);
 
-// модальное окно
+
+// окно регистрации
+const regBtn = document.querySelector('#regBtn');
+const regModal = document.querySelector('.reg-modal');
+const closeRegModalBtn = document.querySelector('.reg-modal__close');
+
+function openRegModal() {
+  document.body.style.overflow = 'hidden';
+  regModal.style.transform = 'translateX(0)';
+}
+
+function closeRegModal() {
+  document.body.style.overflow = '';
+  regModal.style.transform = 'translateX(-100%)';
+}
+
+regBtn.addEventListener('click', openRegModal);
+closeRegModalBtn.addEventListener('click', closeRegModal);
+
+document.addEventListener('keydown', (event) => {
+  if (event.keyCode === 27) {
+    closeRegModal();
+  }
+});
+
+
+// модальное окно фильма (парсинг не работатет)
 const modal = document.querySelector('.modal');
 const modalCloseBtn = document.querySelector('[data-close]');
 const filmItem = document.querySelectorAll('.film__list');
 
+// const API_URL_TRAILER = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/FILMID/videos';
+
+// // const API_URL_SIMILAR = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/FILMID/similars';
+
+
+// async function getMoviesModal(url) {
+//   const resp = await fetch(url, {
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'X-API-KEY': API_KEY,
+//     },
+//   });
+//   const respData = await resp.json();
+
+//   showMoviesModal(respData);
+// }
+
+// function myModalHTML(movie) {
+//   return `
+//   <div class="modal__container">
+//     <div class="movie__frames">
+//       <img class="movie__img" src="${movie.posterUrl}">
+//     </div>
+//     <div class="movie__content">
+//     <div class="movie__rating film__item-rating--${getClassByRate(movie.rating)}">${movie.rating}</div>
+//       <h2 class="movie__title">${movie.nameRu}</h2>
+//       <h4 class="movie__title--original">${movie.nameEn}</h4>
+//       <div class="movie__about">О фильме
+//         <p>Год производства <span>2021</span></p>
+//       </div>
+//       <p class="movie__desc">TEXT</p>
+//       <div>
+//       <p>Сиквелы и приквелы </p>
+//         <img src="" alt="">
+//         <p></p> название
+//       </div>
+//       <div class="movie__trailer">
+//       <p>Трейлер</p>
+//       </div>
+//       <div class="movie__similars">
+//         <p>Похожие фильмы</p>
+//         <div class="movie__similar">
+//           <div class="movie__similar-item">
+//             <img class="movie__similar-img" src="./img/film-poster.jpg" alt="Film name">
+//             <p class="movie__similar-title">Film Name</p>
+//           </div>
+//         </div>
+//       </div>
+//   </div>
+//   </div>`;
+// }
+
+// const movieModalDialog = document.createElement('div');
+// function showMoviesModal(data) {
+//   movieModalDialog.classList.add('modal__dialog');
+
+//   movieModalDialog.innerHTML = myModalHTML(data.films[1]);
+//   modal.appendChild(movieModalDialog);
+// }
+
 function openModal() {
   modal.style.display = 'block';
   document.body.style.overflow = 'hidden';
+  // getMoviesModal(API_URL_POPULAR); //
 }
 
 function closeModal() {
@@ -138,17 +281,7 @@ modal.addEventListener('click', (e) => {
 });
 
 document.addEventListener('keydown', (event) => {
-  if (event.keyCode === 27 && modal.classList.contains('show')) {
+  if (event.keyCode === 27) {
     closeModal();
   }
 });
-
-// function getClassByRateModal(vote) {
-//   if (vote >= 7) {
-//     document.querySelector('.movie__rating').classList.add('movie__rating--good');
-//   } else if (vote >= 5) {
-//     document.querySelector('.movie__rating').classList.add('movie__rating--middle');
-//   } else if (vote < 5) {
-//     document.querySelector('.movie__rating').classList.add('movie__rating--bad');
-//   }
-// }
